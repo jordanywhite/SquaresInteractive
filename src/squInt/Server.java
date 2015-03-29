459fa86cc301f5974476efb074a666cb71b03013
jordanywhite <whitejo16@up.edu>
2015-03-29 03:48:02 -0700

    Removed ClientWindow since some poor soul forgot to push Player.java. Updated User and Server a bit. There is a null pointer exception in DataPort.java that should be dealt with.

--- src/squInt/Server.java
+++ src/squInt/Server.java
@@ -1,8 +1,9 @@
 package squInt;
+
+import java.net.Inet4Address;
 import java.net.InetAddress;
 import java.util.Hashtable;
 import java.util.Queue;
-
 
 /**
  * The User object for the server.
@@ -10,27 +11,89 @@ import java.util.Queue;
  */
 public class Server extends User {
 	
+	public static final int MAX_CLIENTS = 30;
+	public static final int SERVER_ID = 0;
+	/*
+	 * Hash table with User ids connecected to ip addresses for interesting
+	 * information
+	 */
 	Hashtable<Integer, InetAddress> myIpLookup;
+
+	/*
+	 * The unique ids assigned to every client by the server
+	 */
 	int[] userIds;
-	Queue<Action> actions;
-	
-	
+
+	/*
+	 * The id to be assigned to the next client
+	 */
+	int nextId = 1;
+
 	public Server() {
+		// Make a user with all the bells and whistles
 		super();
-		
-		setUserID(0);
-		userIds = new int[30];
+
+		// Make this server a proper user with an id and ip
+		setUserID(SERVER_ID);
+		userIds = new int[MAX_CLIENTS];
 		userIds[0] = getUserID();
-		
 		myIpLookup = new Hashtable<Integer, InetAddress>();
-//		myIpLookup.put(0, myIp);
+		myIpLookup.put(SERVER_ID, this.getIp());
 	}
-	
+
+	/**
+	 * getMasterRoom The one room to rule them all
+	 * 
+	 */
 	public Room getMasterRoom() {
-		return null;
+		return this.getRoom();
 	}
-	
+
+	/**
+	 * getClientIpAddress
+	 */
 	public InetAddress getClientIpAddress(int id) {
-		return null;
+		return myIpLookup.get(id);
+	}
+
+	/**
+	 * registerNewUser Register a new user with the server and assign a user id
+	 * to this new user
+	 * 
+	 * @param addr
+	 *            The ip address of the new client
+	 * @return the id of client assigned by the server
+	 */
+	public int registerUser(InetAddress addr) {
+		if (addr == null) {
+			System.out.println("ERROR: Null IP address");
+			return -1;
+		}
+		myIpLookup.put(nextId, addr);
+		nextId++;
+		return nextId - 1;
+	}
+
+	/**
+	 * removeUser Remove a disconnected client from our server
+	 * 
+	 * @param id
+	 *            the id of the removed client
+	 */
+	public void removeUser(int id) {
+		myIpLookup.remove(id);
+	}
+
+	/**
+	 * requestAction Judge the worth of an action
+	 * 
+	 * @param action
+	 *            on which we shall act on
+	 * @return true if the action is worthy, false if the action is found
+	 *         wanting
+	 */
+	public boolean requestAction(ClientAction action) {
+		// TODO: Reaffirm mechanics
+		return true;
 	}
 }
--- src/squInt/User.java
+++ src/squInt/User.java
@@ -1,5 +1,8 @@
 package squInt;
 
+import java.net.Inet4Address;
+import java.net.InetAddress;
+
 /**
  * One User per host. Contains the local host's data.
  *
@@ -9,18 +12,43 @@ public abstract class User {
 	private int[] otherUsers;
 	private Room room;
 	private GUI gui;
-	
+
 	public User() {
 		this.room = new Room();
 		this.gui = new GUI();
 	}
-	
-	public int getUserID() { return userID; }
-	public Room getRoom() { return room; }
-	public GUI getGui() { return gui; }
-	
-	public void setUserID(int userID) { this.userID = userID; }
-	public void setRoom(Room room) { this.room = room; }
-	public void setGui(GUI gui) { this.gui = gui; }
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public Room getRoom() {
+		return room;
+	}
+
+	public GUI getGui() {
+		return gui;
+	}
+
+	public InetAddress getIp() {
+		// TODO make this method not terrible
+		try{
+			return Inet4Address.getLocalHost();
+		} catch(Exception e) {
+			return null;
+		}
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public void setRoom(Room room) {
+		this.room = room;
+	}
+
+	public void setGui(GUI gui) {
+		this.gui = gui;
+	}
 
 }
